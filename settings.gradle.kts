c45020fe366f0c8880349716f8d2a5d328f3b08a
Kardane <64183602+Kardane@users.noreply.github.com>
2026-09-24 00:47:10 +0900

    fix(t02): allow loader-managed repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,6 @@ plugins {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         mavenCentral()
         maven("https://repo.papermc.io/repository/maven-public/")
